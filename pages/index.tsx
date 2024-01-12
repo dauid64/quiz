0ee303b890a4879d661a148e954288c2bd304adc
dauid64 <carlosdauid64br@gmail.com>
2024-01-11 22:11:32 -0300

    Add variavel de ambiente base_url

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import RespostaModel from '@/model/resposta'
 import { use, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = process.env.BASE_URL
 
 export default function Home() {
   const router = useRouter()
